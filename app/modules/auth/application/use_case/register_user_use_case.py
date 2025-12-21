ed9e5ee9d6fe55365e2aa0d062c78295803233e7
Johnlesis <johnlesis91@gmail.com>
2025-12-21 18:13:49 +0200

    removed not needed verify password

--- app/modules/auth/application/use_case/register_user_use_case.py
+++ app/modules/auth/application/use_case/register_user_use_case.py
@@ -3,20 +3,12 @@ from app.modules.auth.application.request.register_user_request import RegisterU
 from app.modules.auth.domain.entity.user import Roles, User
 from app.modules.auth.domain.repository.user_repository import UserRepositoryInterface
 from argon2 import PasswordHasher
-from argon2.exceptions import VerifyMismatchError
 
 password_hasher = PasswordHasher()
 
 def hash_password(password: str) -> str:
     truncated = password[:72]
     return password_hasher.hash(truncated)
-
-def verify_password(hashed_password: str, plain_password: str) -> bool:
-    try:
-        password_hasher.verify(hashed_password, plain_password)
-        return True
-    except VerifyMismatchError:
-        return False
 
 def user_exists(request: RegisterUserRequest, user_repository: UserRepositoryInterface) -> bool:
     existing_user = user_repository.get_by_username_or_email(

707373d5961ebaa8b8435d26275dad430dd9dcc3
Johnlesis <johnlesis91@gmail.com>
2026-01-24 20:55:55 +0200

    refactor - moved validation logic in service

--- app/services/check_user_superadmin.py
+++ app/services/check_user_superadmin.py
@@ -3,4 +3,4 @@ from app.modules.auth.domain.entity.user import Roles, User
 
 async def check_if_user_superadmin(current_user: User) -> None:
     if current_user.roles != Roles.SUPERADMIN:
-        raise AuthorizationException("Superadmin access required")
+        raise AuthorizationException()
